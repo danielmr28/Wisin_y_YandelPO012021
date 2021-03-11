292f9c9f7ea3cbee08d6144ddfb26d6aaa451d1c
alonso-rb <00081220@uca.edu.sv>
2021-03-10 22:27:42 -0600

    Add: ejemplo1

---
+++ ejemplo1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ejemplo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=0;
+            int b=0;
+            int sum =0;
+            Console.WriteLine("Ingrese un número");
+            a=int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese otro número");
+            b=int.Parse(Console.ReadLine());
+            sum = a + b ;
+            Console.WriteLine("La suma es "+ $"{sum}");
+        
+        }
+    }
+}
